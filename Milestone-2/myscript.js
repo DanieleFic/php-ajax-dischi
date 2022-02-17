9f1bd02a04cd9b150dfbde587ed7a541e6926c05
DanieleFic <reb_ficus22@hotmail.it>
2022-02-17 18:28:26 +0100

    fatto bonus

--- Milestone-2/myscript.js
+++ Milestone-2/myscript.js
@@ -1,7 +1,9 @@
 let app = new Vue({
     el: '#app',
     data: {
-    Arraydischi: []
+    Arraydischi: [],
+    Arraydischigenre : [],
+    valoregenere : null,
     },
 
     mounted:function () {
@@ -16,13 +18,34 @@ let app = new Vue({
             // handle error
             console.log(error);
         })
-        
+        axios.get('http://localhost:8888/php-ajax-dischi/Milestone-2/indexgeneri.php')
+        .then( (risposta2) => {
+            // handle success
+            this.Arraydischigenre = (risposta2.data) 
+            console.log(risposta2.data)
+            console.log(this.Arraydischigenre)
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
     }, 
 
-        methods:{
-            creaEmail: function(){
-            console.log("ciao")
-            }   
+        computed:{
+            filtraGeneri(){
+                if(this.valoregenere == "Null") return this.Arraydischi
+                    return this.Arraydischi.filter( (album) => {
+                    console.log('filtra album');
+                    return album.genre.includes(this.valoregenere) /* || album.author.includes(this.valoreArtisti) */ ;
+                });
         }
+    },
+    /* methods: {
+        cambioValore(event){
+            this.valoregenere = event;
+            console.log(this.valore);  
+        },
+    } */
+    
     
 })
